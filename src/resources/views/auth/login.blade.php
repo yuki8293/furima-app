817e67e1ac18c5a620a3807ab3092d96ce96e8b0
yuki <raiku.yuki@icloud.com>
2026-02-13 20:28:55 +0900

    Fortifyログイン機能実装・ログイン後プロフィール画面遷移

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -9,7 +9,9 @@
     <div class="login-form__heading">
         <h2>ログイン</h2>
     </div>
-    <form class="form">
+    <form class="form" method="POST" action="/login">
+        @csrf
+
         <div class="form__group">
             <div class="form__group-title">
                 <span class="form__label--item">メールアドレス</span>
@@ -45,7 +47,7 @@
         </div>
     </form>
     <div class="register__link">
-        <a class="register__button-submit" href="/register">会員登録の方はこちら</a>
+        <a class="register__button-submit" href="/register">会員登録はこちら</a>
     </div>
 </div>
 @endsection
