73bc63d2ef9cd8fc842c5c0392412ca7a0af8c43
yuki <raiku.yuki@icloud.com>
2026-03-10 21:04:41 +0900

    バリデーションRequestを実装

---
+++ src/app/Http/Requests/AddressRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'postal_code' => 'required|regex:/^\d{3}-\d{4}$/',
+            'address' => 'required',
+        ];
+    }
+}
